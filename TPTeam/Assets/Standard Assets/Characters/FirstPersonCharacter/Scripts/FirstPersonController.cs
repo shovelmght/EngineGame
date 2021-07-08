ab7bd8cdbcde7003dc70229def95ae57d8c43f7a
shovelmght <59151327+shovelmght@users.noreply.github.com>
2021-07-07 21:16:52 -0400

    detectGround

--- TPTeam/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ TPTeam/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -255,5 +255,15 @@ namespace UnityStandardAssets.Characters.FirstPerson
             }
             body.AddForceAtPosition(m_CharacterController.velocity*0.1f, hit.point, ForceMode.Impulse);
         }
+
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("RockFloor"))
+            {
+                Debug.Log("GabIsgay");
+            }
+        }
     }
 }
